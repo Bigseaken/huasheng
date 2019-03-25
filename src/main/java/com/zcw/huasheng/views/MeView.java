6f57ebe26f36cfca0b7e8850115f033f8f5939a2
zhangchaowei <withqianqian@163.com>
2019-03-25 16:23:44 +0800

    fixed:password bug

--- src/main/java/com/zcw/huasheng/views/MeView.java
+++ src/main/java/com/zcw/huasheng/views/MeView.java
@@ -16,23 +16,30 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 @RestController
 @RequestMapping("me")
-@Api(description="我的")
+@Api(description = "我的")
 public class MeView extends AbstractView {
 
     @Autowired
     MemberDao memberDao;
 
-    @ApiOperation(value = "注册用户",notes = "{mobile:手机}")
+    @ApiOperation(value = "注册用户", notes = "{mobile:手机}")
     @PostMapping("addMember")
     public JSONObject addMember(@RequestBody JSONObject member) {
-        if(memberDao.getMember(member) !=null)
-            return getErrResult("手机号码："+member.getString("mobile")+" 已注册了！");
+        if (memberDao.getMember(member) != null)
+            return getErrResult("手机号码：" + member.getString("mobile") + " 已注册了！");
         memberDao.addMember(member);
         return getResult(member);
     }
-    @ApiOperation(value = "根据手机号码获取用户",notes = "mobile=1111")
+
+    @ApiOperation(value = "根据手机号码获取用户", notes = "mobile=1111")
     @GetMapping("getMember")
-    public JSONObject getMember(String mobile){
+    public JSONObject getMember(String mobile, @RequestParam(required = false, defaultValue = "") String password) {
+        if (password == "") {
+            JSONObject params = new JSONObject();
+            params.put("mobile", mobile);
+            params.put("password", password);
+            return getResult(memberDao.getMember(params));
+        }
         return getResult(memberDao.getMemberByMobile(mobile));
     }
 }
